9f177434153fd8719e2ab9fec5cc601776a07a6b
Abdullah Arnous <abdoarnous1@gmail.com>
2026-02-27 00:27:59 +0100

    Added benchmarking logs

--- BenchmarkingEvaluations/createAveragedResults.py
+++ BenchmarkingEvaluations/createAveragedResults.py
@@ -17,7 +17,14 @@ def aggregate_benchmarks(root_dir="logs"):
             continue
 
         for ds_folder in os.listdir(run_path):
-            match = re.search(r'run_[^_]+_(.*?)_\d{4}-\d{2}-\d{2}', ds_folder)
+
+            match = re.search(r'run_Global_Group_Fairness_SP_(.*?)_\d{4}-\d{2}-\d{2}', ds_folder)
+            
+            if not match:
+                match = re.search(r'run_Global_Group_Fairness_Eodd_(.*?)_\d{4}-\d{2}-\d{2}', ds_folder)
+                
+            if not match:
+                match = re.search(r'run_[^_]+_(.*?)_\d{4}-\d{2}-\d{2}', ds_folder)
             
             if match:
                 dataset_id = match.group(1)
@@ -32,10 +39,10 @@ def aggregate_benchmarks(root_dir="logs"):
                             all_observations.append({
                                 "dataset_id": dataset_id,
                                 "run_source": run_folder, 
-                                "accuracy": final_row["accuracy"].values[0],
-                                "balanced_accuracy": final_row["balanced_accuracy"].values[0],
-                                "statistical_parity": final_row["statistical_parity"].values[0],
-                                "equalized_odds": final_row["equalized_odds"].values[0]
+                                "accuracy": final_row["Accuracy"].values[0] if "Accuracy" in final_row else final_row["accuracy"].values[0],
+                                "balanced_accuracy": final_row["balanced_Accuracy"].values[0] if "balanced_Accuracy" in final_row else final_row["balanced_accuracy"].values[0],
+                                "statistical_parity": final_row["Statistical_Parity"].values[0] if "Statistical_Parity" in final_row else final_row["statistical_parity"].values[0],
+                                "equalized_odds": final_row["Equalized_Odds"].values[0] if "Equalized_Odds" in final_row else final_row["equalized_odds"].values[0]
                             })
                     except Exception as e:
                         print(f"Error reading {csv_path}: {e}")
